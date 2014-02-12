935a6070192004c4ea1f6d96dfaabf7b084b4a24
Tiago Moraes <tiagoefmoraes@gmail.com>
2014-02-12 13:45:49 +0000

    revision 2, improve readability

--- ruby/leap/year.rb
+++ ruby/leap/year.rb
@@ -1,5 +1,9 @@
+def evenly_divisible(dividend, divisor)
+  (dividend % divisor).zero?
+end
+
 module Year
   def self.leap?(year)
-    ((year % 4 == 0) and (year % 100 != 0)) or (year % 400 == 0)
+    evenly_divisible(year, 4) and !evenly_divisible(year, 100) or evenly_divisible(year, 400)
   end
 end
